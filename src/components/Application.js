4376faa21063ec6f4a5cbf8a8fa36983a9866875
Benjamin Shaw <shaw.j.benjamin@gmail.com>
2020-06-13 21:33:52 -0400

    Debug; add transtitions, save, delete

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,57 +7,6 @@ import DayList from "components/DayList";
 import Appointment from "components/Appointment";
 import { getAppointmentsForDay, getInterviewersForDay, getInterview } from "helpers/selectors";
 
-/* const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-    // interview: {
-    //   student: "Bobby Boi",
-    //   interviewer: {
-    //     id: 2,
-    //     name: "Tori Malcolm",
-    //     avatar: "https://i.imgur.com/Nmx0Qxo.png"
-    //   }
-    // }
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-    interview: {
-      student: "Danny Needshelp",
-      interviewer: {
-        id: 1,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Rea Ct",
-      interviewer: {
-        id: 5,
-        name: "Sven Jones",
-        avatar: "https://i.imgur.com/twYrpay.jpg",
-      }
-    }
-  }
-]; */
-
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
@@ -65,8 +14,84 @@ export default function Application(props) {
     appointments: {}
   });
 
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    
+    {console.log('appointment:', appointment);
+    console.log('state.appointments[id].id:', state.appointments[id].id)
+    console.log('id:', id)
+    console.log('appointment.interview:', appointment.interview) // this is the interview data, same thing as just interview
+    console.log('interview:', interview)
+    console.log('appointments:', appointments) // this is the list of 25 interview slots with most interviews set to null
+  }
+    
+  
+  
+    axios.put(`/api/appointments/${id}`, { interview }) // update what, and to what?
+      .then(response => {
+        console.log(response);
+      })
+      .catch((e) => {console.log(e)})
+      
+      {// console.log(id, interview, appointment);
+      
+      // PROBLEM: make an axios PUT request to /api/appointments/:id
+      // with interview data in the body
+      // to update database with the appointment data
+      // QUESTIONS
+      // 1. does :id need to refer to the id argument? $(id) --> try both
+      //    - looks like data fetching or calling an API counts as a side effect in React
+      // 2. do I need to wrap the axios call in useEffect? --> look up useEffect
+      //    - A. looks like YES
+      //    - except maybe not because it's inside a normal function (not a custom Hook or React function)
+      // 3. do I need to use a Promise?
+      // 4. How do I put the interview data in the body? Using the interview argument?
+      
+      // STEPS
+      // 1. Make the request --> should receive 204 No Content response
+      
+      // useEffect: tells React your component needs to do something after render
+      // React will remember function passed ('effect') and call it later after performing the DOM updates
+      // useEffect called inside components gives it access to the props and component scope
+      // useEffect runs after every render (i.e. 'after render')
+}
+      
+      setState({ ...state, appointments });
+    
+  }
+
+  function cancelInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    axios.delete(`/api/appointments/${id}`, { interview: null })
+      .then(response => {
+        console.log(response);
+      })
+      .catch((e) => {console.log(e)});
+
+      setState({ ...state, appointments });
+
+
+  }
+
   const setDay = day => setState({ ...state, day });
- 
+
   useEffect(() => {
     Promise.all([
       axios.get(`/api/days`),
@@ -74,42 +99,52 @@ export default function Application(props) {
       axios.get(`/api/interviewers`)
 
     ]).then((all) => {
-      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
-      console.log(all);
+      setState(prev => ({ ...prev, 
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data 
+      }));
     });
   }, [])
 
   const interviewers = getInterviewersForDay(state, state.day);
-  // const interview = getInterview(state, state.appointments.interview);
 
   return (
     <main className="layout">
-      <section className="sidebar"> 
-      <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu">
-    <DayList
-  days={state.days}
-  day={state.day}
-  setDay={setDay}
-/>
-</nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
+      <section className="sidebar">
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList
+            days={state.days}
+            day={state.day}
+            setDay={setDay}
+          />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
         <ul>
-        {getAppointmentsForDay(state, state.day).map((appointment) =>
-          <Appointment key={appointment.id} interviewList={interviewers} interview={getInterview(state, appointment.interview)} id={appointment.id} time={appointment.time} />
-        )}
-        <Appointment key="last" time="5pm" />
+          {getAppointmentsForDay(state, state.day).map((appointment) =>
+            <Appointment 
+            key={appointment.id}
+            interviewList={interviewers}
+            interview={getInterview(state, appointment.interview)}
+            id={appointment.id}
+            time={appointment.time}
+            bookInterview={bookInterview}
+            cancelInterview={cancelInterview}
+            />
+          )}
+          <Appointment key="last" time="5pm" />
         </ul>
       </section>
     </main>
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,10 +7,15 @@ import Show from "./Show";
 import Empty from "./Empty";
 import useVisualMode from "hooks/useVisualMode";
 import Form from "./Form";
+import Status from "./Status";
+import Confirm from "./Confirm";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
+const SAVING = "SAVING";
+const DELETING = "DELETING";
+const CONFIRM = "CONFIRM";
 
 
 
@@ -18,6 +23,28 @@ export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
   props.interview ? SHOW : EMPTY
   );
+
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    console.log(name, interviewer);
+    transition(SAVING);
+    props.bookInterview(props.id, interview);
+    transition(SHOW);
+    // transition to SHOW
+  }
+
+  function deleteIntv(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    transition(DELETING);
+    props.cancelInterview(props.id, interview);
+    transition(EMPTY);
+  }
 
   return (
     <article className="appointment">
@@ -28,11 +55,20 @@ export default function Appointment(props) {
       student={props.interview.student}
       interviewer={props.interview.interviewer}
       onEdit={props.onEdit}
-      onDelete={props.onDelete}
+      onDelete={() => transition(CONFIRM)}
       interviewers={props.interviewList}
     />
   )}
-  {mode === CREATE && <Form name={props.name} interviewers={[]} onSave={props.onSave} onCancel={() => back()} />}
+  {mode === CREATE && 
+    <Form 
+      name={props.name}
+      interviewers={props.interviewList}
+      onSave={save}
+      onCancel={back}
+    />}
+
+    {mode === SAVING && <Status />}
+    {mode === CONFIRM && <Confirm onConfirm={deleteIntv} onCancel={back}/>}
       {/* { props.interview ? <Show student={props.interview.student} interviewer={props.interviewList[props.interview.interviewer]}/> : <Empty /> } */}
     </article>
   )
